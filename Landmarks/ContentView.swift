5613962b9bf07d69011a6c05e2f4f322bf68c89e
Chhuon Ek <chhuon@slash.co>
2020-03-27 04:10:48 +0700

    Added test

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                 HStack {
                     Text("Joshua Tree National Park")
                     Spacer()
-                    Text("California")
+                    Text("California Test")
                 }
             }
             .padding()
